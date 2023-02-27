bcdc8cdb956636287130d004bdc57bf308eebf81
MitasovArthur <mitasovartur2@gmail.com>
2023-02-28 01:13:37 +0200

    add method sRectangle add main for check class Calcul

---
+++ src/Calculator/main.java
@@ -0,0 +1,10 @@
+package Calculator;
+
+public class main {
+    public static void main(String[] args) {
+        Calcul.sCube(5);
+        Calcul.sRectangle(5, 5);
+        Calcul.sScircle(5);
+
+    }
+}
